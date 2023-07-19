b9844b996d2213d90c3d2231f0db2caf10bf0764
Vinicius Aderaldo <138501694+viniciusaderaldo@users.noreply.github.com>
2023-07-19 12:08:16 -0300

    Modificações para a responsividade de dispositivos menores

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
+const botaoMenu = document.querySelector(".menuAbrir");
+const menuNav = document.querySelector(".menuMobile");
+const cabecalho = document.querySelector(".cabecalho");
 
+botaoMenu.addEventListener('click', () => {
+    menuNav.classList.toggle("ativo");
+    cabecalho.classList.toggle("menuAberto");
+});
